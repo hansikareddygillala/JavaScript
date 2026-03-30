734a9a7ef500d2320959f8322dd68ddf1aeaca58
G Hansika Reddy <hansikagillala1111@gmail.com>
2026-03-30 09:19:05 +0530

    30/3/2026 9:19 PM

--- LESSONS/11-To_do_List.js
+++ LESSONS/11-To_do_List.js
@@ -5,7 +5,20 @@ renderTodoList();
 function renderTodoList() {
   let todoListHTML = ``;
   for (let i=0; i < todoList.length; i++) {
-    const html = `<p>${todoList[i]}</p>`;
+    const todoObject = todoList[i];
+    //const name = todoObject.name;
+    //const dueDate = todoObject.dueDate;
+    const { name, dueDate } = todoObject;
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button 
+      onclick="
+        todoList.splice(${i}, 1); 
+        renderTodoList();"
+      class="delete-todo-button"
+      >Delete</button>
+    `;
     todoListHTML += html;
   }
   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
@@ -15,8 +28,15 @@ function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
   const name = inputElement.value;
 
-  todoList.push(name);
-  console.log(todoList);
+  const dueDateInputElement = document.querySelector(".js-due-date-input");
+  const dueDate = dueDateInputElement.value;
+  todoList.push({
+    //name : name,
+    //dueDate : dueDate
+    //same as above
+    name,
+    dueDate
+  });
   inputElement.value = '';
 
   renderTodoList();
